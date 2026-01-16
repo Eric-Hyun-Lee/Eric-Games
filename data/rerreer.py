5a6ed39b7878a492ca7e310d75756c88589eaa1c
Eric-Hyun-Lee <superturboeric@icloud.com>
2026-01-15 20:28:17 -0600

    Moved a bunch of random files and deleted some
    moved to eric code

--- data/rerreer.py
+++
@@ -1,8 +0,0 @@
-UserInput  = input("Type in a number: ") # Ask for the user to enter a number
-ReplacementBackwards = ""
-for i in range(len(UserInput)):
-    ReplacementBackwards+=UserInput[len(UserInput)-i-1]
-    
-print(int(ReplacementBackwards))
-for i in range(2,45,3):
-    print(i)
